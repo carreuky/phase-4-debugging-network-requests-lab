4e2bc9ba61207ad1c6fe3981a26ad92cbf46c4a4
carreuky <carreuky@gmail.com>
2022-09-27 12:10:28 +0300

    commit tree 1a97cb13eb62b29e632a5f917a6124ed856bf583

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,8 +7,20 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
+  end
+
+  def show
+    toy = Toy.find_by(id: params[:id])
+    render json: toy
+
+  end
+  
+  def increment_likes
+    toy = Toy.find_by(id: params[:id])
+    toy.update(likes: toy.likes + 1)
+    render json: toy, status: :accepted
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :toys, only: [:index, :create, :update]
+  resources :toys, only: [:index, :show, :destroy, :create, :update]
+  patch "/toys/:id/likes", to: "toys#increment_likes"
+
 end
